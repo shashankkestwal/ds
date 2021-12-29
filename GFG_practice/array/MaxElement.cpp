56b260631cea6648470527ddb14e35cf8e2887dd
Shashank <shashankkestwal777@gmail.com>
2021-12-30 00:21:14 +0530

    simple max program

---
+++ GFG_practice/array/MaxElement.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main(){
+	int size;
+	cout << "array size : ";
+	cin >> size ;
+	int array[size];
+	for (int i = 0 ; i < size; i++) {
+		cin >> array[i];
+	}
+	int max = INT_MIN;
+	for (int i = 0; i < size; ++i)
+	{
+		if(array[i] > max) {
+			max = array[i];
+		}
+	}
+	cout << "max element is :" << max << endl;
+ 	return 0;
+}
